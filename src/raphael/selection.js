74baacd38321ad1a10934b95f466a40031ad88c6
Clint Tseng <clint@dontexplain.com>
2012-06-01 16:05:21 -0700

    implement selection.remove() so that one can do .exit().remove() Conflicts: 	d3.v2.js 	d3.v2.min.js 	src/raphael/selection.js

--- src/raphael/selection.js
+++ src/raphael/selection.js
@@ -355,6 +355,18 @@ d3_raphael_selectionPrototype.transition = function() {
 
     return transition;
 };
+/**
+ * Removes each instance of the selection element.
+ *
+ * @see <a href="https://github.com/mbostock/d3/wiki/Selections#wiki-remove">d3.selection.remove()</a>
+ *
+ * @param {Array} value
+ * @return {D3RaphaelSelection} this
+ *
+ * @function
+ * @name D3RaphaelSelection#remove
+ */
+d3_raphael_selectionPrototype.remove = d3_selectionPrototype.remove;
 
 d3_raphael_selectionPrototype.style = throw_raphael_not_supported;
 d3_raphael_selectionPrototype.html = throw_raphael_not_supported;
